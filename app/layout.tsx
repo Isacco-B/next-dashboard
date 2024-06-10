ce8a848da3401e842547936c5aade6d8ffcf2d38
Isacco-B <git@isaccobertoli.com>
2024-06-10 14:45:25 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import { inter } from './ui/fonts';
+import './ui/global.css';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
